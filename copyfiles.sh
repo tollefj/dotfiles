7407408180f76c092d190f91f083c2307ce33144
Tollef Jørgensen <tollefj@gmail.com>
2017-07-16 12:01:44 +0200

    Removed old shell files

--- copyfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat "copying vimrc"
-cp ~/.vimrc .
-cat "copying gitconfig"
-cp ~/.gitconfig .
-cat "copying bash_profile"
-cp ~/.bash_profile .
-exit;
--- pastefiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cat "update vimrc"
-cp .vimrc ~/
-cat "update gitconfig"
-cp .gitconfig ~/ 
-cat "update bash_profile"
-cp .bash_profile ~/
-exit;
-# run chmod +x pastefiles.sh
